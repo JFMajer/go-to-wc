18d5d00f7806cbd95576af4800dddc1d85aa0a6f
JFMajer <jakubmajer@live.com>
2023-12-30 11:34:33 +0100

    reading bytes

--- main.go
+++ main.go
@@ -2,19 +2,34 @@ package main
 
 import (
 	"bufio"
+	"flag"
 	"fmt"
 	"io"
 	"os"
 )
 
 func main() {
-	wc := count(os.Stdin)
+
+	// Defining boolean flag -l to count lines instead of words
+	lines := flag.Bool("l", false, "Count lines")
+
+	// Defining boolean flag -b to count bytes instead of words
+	bytes := flag.Bool("b", false, "Count bytes")
+	flag.Parse()
+
+	wc := count(os.Stdin, *lines, *bytes)
 	fmt.Println(wc)
 }
 
-func count(r io.Reader) int {
+func count(r io.Reader, countLines bool, countBytes bool) int {
 	scanner := bufio.NewScanner(r)
-	scanner.Split(bufio.ScanWords)
+	if countLines {
+		scanner.Split(bufio.ScanLines)
+	} else if countBytes {
+		scanner.Split(bufio.ScanBytes)
+	} else {
+		scanner.Split(bufio.ScanWords)
+	}
 
 	wc := 0
 	for scanner.Scan() {
@@ -23,6 +38,7 @@ func count(r io.Reader) int {
 
 	if err := scanner.Err(); err != nil {
 		fmt.Fprintf(os.Stderr, "reading input: %v\n", err)
+		return 0
 	}
 
 	return wc
--- main_test.go
+++ main_test.go
@@ -9,8 +9,28 @@ func TestWordCount(t *testing.T) {
 	b := bytes.NewBufferString("An old silent pond A frog jumps into the pond— Splash! Silence again.\n")
 
 	exp := 13
-	res := count(b)
+	res := count(b, false, false)
 	if res != exp {
 		t.Errorf("Expected %d words, got %d", exp, res)
+	}
+}
+
+func TestCountLines(t *testing.T) {
+	b := bytes.NewBufferString("An old silent pond\n A frog jumps into the pond\n— Splash! Silence again.\n")
+
+	exp := 3
+	res := count(b, true, false)
+	if res != exp {
+		t.Errorf("Expected %d lines, got %d", exp, res)
+	}
+}
+
+func TestCountBytes(t *testing.T) {
+	b := bytes.NewBufferString("An old silent pond A frog jumps into the pond— Splash! Silence again.\n")
+
+	exp := 72
+	res := count(b, false, true)
+	if res != exp {
+		t.Errorf("Expected %d bytes, got %d", exp, res)
 	}
 }
